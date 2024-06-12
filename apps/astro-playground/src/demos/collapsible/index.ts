5be93bfa7b2223582b6f4faa6da66ec5ba171f1d
ZAHON <konrad.zahon@gmail.com>
2024-06-12 19:13:29 +0200

    feat(astro-playground): update demos structure

---
+++ apps/astro-playground/src/demos/collapsible/index.ts
@@ -0,0 +1,10 @@
+export { DisabledClosedByDefault } from './disabled-closed-by-default';
+export { DisabledOpenByDefault } from './disabled-open-by-default';
+export { UncontrolledClosedByDefault } from './uncontrolled-closed-by-default';
+export { UncontrolledOpenByDefault } from './uncontrolled-open-by-default';
+export { ControlledClosedByDefault } from './controlled-closed-by-default';
+export { ControlledOpenByDefault } from './controlled-open-by-default';
+export { AnimatedUncontrolledClosedByDefault } from './animated-uncontrolled-closed-by-default';
+export { AnimatedUncontrolledOpenByDefault } from './animated-uncontrolled-open-by-default';
+export { AnimatedControlledClosedByDefault } from './animated-controlled-closed-by-default';
+export { AnimatedControlledOpenByDefault } from './animated-controlled-open-by-default';
