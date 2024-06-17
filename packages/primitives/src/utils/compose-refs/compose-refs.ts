e8e55dc9c6aad6e54ba918c67a9f9ddc18166b4b
ZAHON <konrad.zahon@gmail.com>
2024-06-18 01:08:10 +0200

    feat(primitives)!: change `composeRefs` utility function API

--- packages/primitives/src/utils/compose-refs/compose-refs.ts
+++ packages/primitives/src/utils/compose-refs/compose-refs.ts
@@ -1,7 +1,7 @@
 import type { Signal } from '@builder.io/qwik';
 import type { Ref } from './compose-refs.types';
 
-export const composeRefs = <T extends Element = Element>(...refs: Ref<T>[]) => {
+export const composeRefs = <T extends Element = Element>(refs: Ref<T>[]) => {
   return (node: T) =>
     refs.forEach((ref) => {
       if (typeof ref === 'function') {
