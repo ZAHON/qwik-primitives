311ed154b2414c23919d4df795671f4aece1d600
ZAHON <konrad.zahon@gmail.com>
2024-08-26 21:46:31 +0200

    feat(primitives): change `CollapsibleContext` name from `collapsible-context` to `qwik-primitives-collapsible-context`

--- packages/primitives/src/components/collapsible/collapsible-context/collapsible-context.tsx
+++ packages/primitives/src/components/collapsible/collapsible-context/collapsible-context.tsx
@@ -1,4 +1,4 @@
 import type { CollapsibleContextValue } from './collapsible-context.types';
 import { createContextId } from '@builder.io/qwik';
 
-export const CollapsibleContext = createContextId<CollapsibleContextValue>('collapsible-context');
+export const CollapsibleContext = createContextId<CollapsibleContextValue>('qwik-primitives-collapsible-context');
