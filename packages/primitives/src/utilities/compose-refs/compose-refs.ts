f8699cc42221f14c1ada231463620159f10dc60e
ZAHON <konrad.zahon@gmail.com>
2024-09-12 02:06:15 +0200

    feat(primitives): improve `composeRefs` utility function to works with components provided by `as` prop

--- packages/primitives/src/utilities/compose-refs/compose-refs.ts
+++ packages/primitives/src/utilities/compose-refs/compose-refs.ts
@@ -1,13 +1,16 @@
-import type { Signal } from '@builder.io/qwik';
 import type { Ref } from './compose-refs.types';
+import type { Signal } from '@builder.io/qwik';
+import { $ } from '@builder.io/qwik';
 
 export const composeRefs = <T extends Element = Element>(refs: Ref<T>[]) => {
-  return (node: T) =>
-    refs.forEach((ref) => {
+  return $((node: T) =>
+    // eslint-disable-next-line qwik/valid-lexical-scope
+    refs?.forEach((ref) => {
       if (typeof ref === 'function') {
         ref(node);
       } else if (ref !== undefined) {
         (ref as Signal<T>).value = node;
       }
-    });
+    })
+  );
 };
