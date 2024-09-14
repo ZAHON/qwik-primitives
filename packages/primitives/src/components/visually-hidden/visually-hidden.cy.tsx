999e40a0b0d0dfe710b1ba3238223aa7e54231d6
ZAHON <konrad.zahon@gmail.com>
2024-09-14 12:06:00 +0200

    test(primitives): update tests for `VisuallyHidden` component

--- packages/primitives/src/components/visually-hidden/visually-hidden.cy.tsx
+++ packages/primitives/src/components/visually-hidden/visually-hidden.cy.tsx
@@ -16,6 +16,32 @@ describe('VisuallyHidden', () => {
       cy.get(`[data-testid="${VISUALLY_HIDDEN_TESTID}"]`).should('have.prop', 'tagName').should('eq', 'DIV');
     });
 
+    it('should contain passed children', () => {
+      const VISUALLY_HIDDEN_TEXT = 'VISUALLY_HIDDEN_TEXT';
+
+      cy.mount(<VisuallyHidden.Root data-testid={VISUALLY_HIDDEN_TESTID}>{VISUALLY_HIDDEN_TEXT}</VisuallyHidden.Root>);
+      cy.get(`[data-testid="${VISUALLY_HIDDEN_TESTID}"]`).should('have.text', VISUALLY_HIDDEN_TEXT);
+    });
+
+    it('should have visually hidden style', () => {
+      cy.mount(<VisuallyHidden.Root data-testid={VISUALLY_HIDDEN_TESTID} />);
+      cy.get(`[data-testid="${VISUALLY_HIDDEN_TESTID}"]`)
+        .invoke('attr', 'style')
+        .should(
+          'contain',
+          `position: absolute; border: 0px; width: 1px; height: 1px; padding: 0px; margin: -1px; overflow: hidden; clip: rect(0px, 0px, 0px, 0px); white-space: nowrap; overflow-wrap: normal;`
+        );
+    });
+
+    it('should have inline style provided via style prop', () => {
+      const VISUALLY_HIDDEN_COLOR = 'rgb(1, 2, 3)';
+
+      cy.mount(<VisuallyHidden.Root style={{ color: VISUALLY_HIDDEN_COLOR }} data-testid={VISUALLY_HIDDEN_TESTID} />);
+      cy.get(`[data-testid="${VISUALLY_HIDDEN_TESTID}"]`)
+        .invoke('attr', 'style')
+        .should('contain', `color: ${VISUALLY_HIDDEN_COLOR}`);
+    });
+
     it('should have attribute data-qwik-primitives-visually-hidden-root with empty value', () => {
       cy.mount(<VisuallyHidden.Root data-testid={VISUALLY_HIDDEN_TESTID} />);
       cy.get(`[data-testid="${VISUALLY_HIDDEN_TESTID}"]`).should(
@@ -23,6 +49,16 @@ describe('VisuallyHidden', () => {
         'data-qwik-primitives-visually-hidden-root',
         ''
       );
+    });
+
+    it('should have attribute data-scope with value "visually-hidden"', () => {
+      cy.mount(<VisuallyHidden.Root data-testid={VISUALLY_HIDDEN_TESTID} />);
+      cy.get(`[data-testid="${VISUALLY_HIDDEN_TESTID}"]`).should('have.attr', 'data-scope', 'visually-hidden');
+    });
+
+    it('should have attribute data-part with value "root"', () => {
+      cy.mount(<VisuallyHidden.Root data-testid={VISUALLY_HIDDEN_TESTID} />);
+      cy.get(`[data-testid="${VISUALLY_HIDDEN_TESTID}"]`).should('have.attr', 'data-part', 'root');
     });
   });
 });
