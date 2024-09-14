9827b1ca9707cab4d004b626505b8d1e5c6c91e7
ZAHON <konrad.zahon@gmail.com>
2024-09-14 03:21:26 +0200

    test(primitives): update tests for `Label` component

--- packages/primitives/src/components/label/label.cy.tsx
+++ packages/primitives/src/components/label/label.cy.tsx
@@ -2,34 +2,48 @@ import { Primitive } from '@/components';
 import * as Label from '.';
 
 const LABEL_ROOT_TESTID = 'LABEL_ROOT_TESTID';
-const LABEL_ROOT_TEXT = 'LABEL_ROOT_TEXT';
 
 describe('Label', () => {
   describe('Root', () => {
     it('should be <label> element', () => {
-      cy.mount(<Label.Root data-testid={LABEL_ROOT_TESTID}>{LABEL_ROOT_TEXT}</Label.Root>);
+      cy.mount(<Label.Root data-testid={LABEL_ROOT_TESTID} />);
       cy.get(`[data-testid="${LABEL_ROOT_TESTID}"]`).should('have.prop', 'tagName').should('eq', 'LABEL');
     });
 
     it('should be element provided via as prop', () => {
-      cy.mount(
-        <Label.Root as={Primitive.div} data-testid={LABEL_ROOT_TESTID}>
-          {LABEL_ROOT_TEXT}
-        </Label.Root>
-      );
+      cy.mount(<Label.Root as={Primitive.div} data-testid={LABEL_ROOT_TESTID} />);
       cy.get(`[data-testid="${LABEL_ROOT_TESTID}"]`).should('have.prop', 'tagName').should('not.eq', 'LABEL');
       cy.get(`[data-testid="${LABEL_ROOT_TESTID}"]`).should('have.prop', 'tagName').should('eq', 'DIV');
     });
 
     it('should prevent text selection when double clicking', () => {
+      const LABEL_ROOT_TEXT = 'LABEL_ROOT_TEXT';
+
       cy.mount(<Label.Root data-testid={LABEL_ROOT_TESTID}>{LABEL_ROOT_TEXT}</Label.Root>);
       cy.get(`[data-testid=${LABEL_ROOT_TESTID}]`).realClick({ clickCount: 2 });
       cy.window().then((window) => expect(window.getSelection()?.toString()).to.eq(''));
     });
 
-    it('should have attribute data-qwik-primitives-label-root with empty value', () => {
+    it('should contain passed children', () => {
+      const LABEL_ROOT_TEXT = 'LABEL_ROOT_TEXT';
+
       cy.mount(<Label.Root data-testid={LABEL_ROOT_TESTID}>{LABEL_ROOT_TEXT}</Label.Root>);
+      cy.get(`[data-testid=${LABEL_ROOT_TESTID}]`).should('have.text', LABEL_ROOT_TEXT);
+    });
+
+    it('should have attribute data-qwik-primitives-label-root with empty value', () => {
+      cy.mount(<Label.Root data-testid={LABEL_ROOT_TESTID} />);
       cy.get(`[data-testid="${LABEL_ROOT_TESTID}"]`).should('have.attr', 'data-qwik-primitives-label-root', '');
+    });
+
+    it('should have attribute data-scope with value "label"', () => {
+      cy.mount(<Label.Root data-testid={LABEL_ROOT_TESTID} />);
+      cy.get(`[data-testid="${LABEL_ROOT_TESTID}"]`).should('have.attr', 'data-scope', 'label');
+    });
+
+    it('should have attribute data-part with value "root"', () => {
+      cy.mount(<Label.Root data-testid={LABEL_ROOT_TESTID} />);
+      cy.get(`[data-testid="${LABEL_ROOT_TESTID}"]`).should('have.attr', 'data-part', 'root');
     });
   });
 });
