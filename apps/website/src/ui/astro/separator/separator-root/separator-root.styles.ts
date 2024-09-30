7a823f4367821b563dd59d0af2ce7fa142cc01f4
ZAHON <konrad.zahon@gmail.com>
2024-09-30 23:23:43 +0200

    feat(website): add `Separator` ui astro component

---
+++ apps/website/src/ui/astro/separator/separator-root/separator-root.styles.ts
@@ -0,0 +1,10 @@
+import { cva } from 'class-variance-authority';
+
+export const separatorRootStyles = cva(['shrink-0', 'bg-default-6'], {
+  variants: {
+    orientation: {
+      horizontal: ['h-px', 'w-full'],
+      vertical: ['h-full', 'w-px'],
+    },
+  },
+});
