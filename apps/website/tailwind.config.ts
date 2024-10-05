adeacb8a1f57a7f488f5ce5e568764f4fa461160
ZAHON <konrad.zahon@gmail.com>
2024-10-05 19:01:38 +0200

    build(website): add `@fontsource/iosevka` package as dependency

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -24,6 +24,7 @@ export default {
       },
       fontFamily: {
         sans: ['Geist Sans', ...fontFamily.sans],
+        mono: ['Iosevka', ...fontFamily.mono],
       },
       keyframes: {
         'mobile-menu-content-show': {
