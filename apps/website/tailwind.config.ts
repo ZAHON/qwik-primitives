5798cc8dcf4325441e503c9a8dddadbc05a08eab
ZAHON <konrad.zahon@gmail.com>
2024-10-06 18:25:04 +0200

    chore(website): add custom set of line height utilities in tailwind css config

--- apps/website/tailwind.config.ts
+++ apps/website/tailwind.config.ts
@@ -87,6 +87,17 @@ export default {
           },
         ],
       },
+      lineHeight: {
+        1: '1rem',
+        2: '1.25rem',
+        3: '1.5rem',
+        4: '1.625rem',
+        5: '1.75rem',
+        6: '1.875rem',
+        7: '2.25rem',
+        8: '2.5rem',
+        9: '3.75rem',
+      },
       fontFamily: {
         sans: ['Geist Sans', ...fontFamily.sans],
         mono: ['Iosevka', ...fontFamily.mono],
