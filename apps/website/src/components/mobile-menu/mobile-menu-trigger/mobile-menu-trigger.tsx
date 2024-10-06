3fcb7f3e0043368401a70414e7f32dd4dce1d84d
ZAHON <konrad.zahon@gmail.com>
2024-10-06 23:50:15 +0200

    feat(website): update `MobileMenuTrigger` component

--- apps/website/src/components/mobile-menu/mobile-menu-trigger/mobile-menu-trigger.tsx
+++ apps/website/src/components/mobile-menu/mobile-menu-trigger/mobile-menu-trigger.tsx
@@ -1,14 +1,24 @@
 import { component$, useContext } from '@builder.io/qwik';
 import { Dialog } from 'qwik-primitives';
+import { twm } from '@/utilities/twm';
 import { HamburgerMenuIcon } from '@/ui/qwik/icons/hamburger-menu-icon';
+import { buttonStyles } from '@/styles/button';
 import { mobileMenuTriggerStyles } from './mobile-menu-trigger.styles';
 import { MobileMenuContext } from '../mobile-menu-context';
+
+const iconButtonStyles = buttonStyles({
+  size: '2',
+  shape: 'icon',
+  variant: 'ghost',
+  color: 'default',
+  highContrast: false,
+});
 
 export const MobileMenuTrigger = component$(() => {
   const { name } = useContext(MobileMenuContext);
 
   return (
-    <Dialog.Trigger class={mobileMenuTriggerStyles()}>
+    <Dialog.Trigger class={twm(iconButtonStyles, mobileMenuTriggerStyles())}>
       <HamburgerMenuIcon height={16} width={16} />
       <span class="sr-only">Open {name} navigation</span>
     </Dialog.Trigger>
