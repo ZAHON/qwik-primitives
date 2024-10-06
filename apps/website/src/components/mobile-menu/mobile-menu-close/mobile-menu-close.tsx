22cfdf600091cebb47cc28cee7823802b8152206
ZAHON <konrad.zahon@gmail.com>
2024-10-06 23:53:38 +0200

    feat(website): update `MobileMenuClose` component

--- apps/website/src/components/mobile-menu/mobile-menu-close/mobile-menu-close.tsx
+++ apps/website/src/components/mobile-menu/mobile-menu-close/mobile-menu-close.tsx
@@ -1,14 +1,23 @@
 import { component$, useContext } from '@builder.io/qwik';
 import { Dialog } from 'qwik-primitives';
+import { twm } from '@/utilities/twm';
 import { CrossIcon } from '@/ui/qwik/icons/cross-icon';
-import { mobileMenuCloseStyles } from './mobile-menu-close.styles';
+import { buttonStyles } from '@/styles/button';
 import { MobileMenuContext } from '../mobile-menu-context';
+
+const iconButtonStyles = buttonStyles({
+  size: '2',
+  shape: 'icon',
+  variant: 'ghost',
+  color: 'default',
+  highContrast: false,
+});
 
 export const MobileMenuClose = component$(() => {
   const { name } = useContext(MobileMenuContext);
 
   return (
-    <Dialog.Close data-qwik-primitives-auto-focus="" class={mobileMenuCloseStyles()}>
+    <Dialog.Close data-qwik-primitives-auto-focus="" class={twm(iconButtonStyles)}>
       <CrossIcon height={16} width={16} />
       <span class="sr-only">Close {name} navigation</span>
     </Dialog.Close>
