25044f23d85325e73918b96428b4b8f7ed9eb8fe
ZAHON <konrad.zahon@gmail.com>
2024-10-06 23:29:39 +0200

    feat(website): update styles for `PageHeaderDocumentationLink` component

--- apps/website/src/components/page-header/page-header-documentation-link/page-header-documentation-link.styles.ts
+++ apps/website/src/components/page-header/page-header-documentation-link/page-header-documentation-link.styles.ts
@@ -1,6 +1,6 @@
 import { cva } from 'class-variance-authority';
 
-export const pageHeaderDocumentationLinkStyles = cva(['text-sm', 'select-none', 'hidden', 'lg:block'], {
+export const pageHeaderDocumentationLinkStyles = cva(['text-2', 'select-none', 'hidden', 'lg:block'], {
   variants: {
     active: {
       true: ['underline', 'text-default-12', 'decoration-default-6', 'focus-visible:no-underline'],
