e843d4ec56b237ef25d4722a0d2b58a9267e1499
ZAHON <konrad.zahon@gmail.com>
2024-10-06 18:06:21 +0200

    feat(website): integrate `twm` utility function to work with custom set of font size utilities

--- apps/website/src/utilities/twm/twm.ts
+++ apps/website/src/utilities/twm/twm.ts
@@ -1,3 +1,9 @@
 import { extendTailwindMerge } from 'tailwind-merge';
 
-export const twm = extendTailwindMerge({});
+export const twm = extendTailwindMerge({
+  override: {
+    classGroups: {
+      'font-size': ['text-1', 'text-2', 'text-3', 'text-4', 'text-5', 'text-6', 'text-7', 'text-8', 'text-9'],
+    },
+  },
+});
