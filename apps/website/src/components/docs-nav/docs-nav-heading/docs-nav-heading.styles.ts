9809c459e406482331d34fe7168c3c23c5a687d5
ZAHON <konrad.zahon@gmail.com>
2024-10-06 23:22:10 +0200

    feat(website): update styles for `DocsNavHeading` component

--- apps/website/src/components/docs-nav/docs-nav-heading/docs-nav-heading.styles.ts
+++ apps/website/src/components/docs-nav/docs-nav-heading/docs-nav-heading.styles.ts
@@ -1,3 +1,3 @@
 import { cva } from 'class-variance-authority';
 
-export const docsNavHeadingStyles = cva(['px-3', 'font-bold', 'text-base', 'text-default-12', 'lg:text-sm']);
+export const docsNavHeadingStyles = cva(['px-3', 'font-bold', 'text-3', 'text-default-12', 'lg:text-2']);
