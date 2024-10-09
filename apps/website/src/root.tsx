a78b3562c4693e8b0ad1dc11b942a4df6af35d37
ZAHON <konrad.zahon@gmail.com>
2024-10-09 16:14:31 +0200

    feat(website): change way to import fonts

--- apps/website/src/root.tsx
+++ apps/website/src/root.tsx
@@ -3,12 +3,6 @@ import { isDev } from '@builder.io/qwik/build';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
 import { RouterHead } from './components/router-head';
 
-import '@fontsource/iosevka';
-
-import '@fontsource/geist-sans/400.css';
-import '@fontsource/geist-sans/500.css';
-import '@fontsource/geist-sans/700.css';
-
 import '@/styles/globals.css';
 
 export default component$(() => {
@@ -19,7 +13,7 @@ export default component$(() => {
         {!isDev && <link rel="manifest" href={`${import.meta.env.BASE_URL}manifest.json`} />}
         <RouterHead />
       </head>
-      <body class="bg-background text-foreground min-h-dvh overflow-x-hidden">
+      <body class="min-h-dvh overflow-x-hidden bg-background text-foreground">
         <RouterOutlet />
         {!isDev && <ServiceWorkerRegister />}
       </body>
