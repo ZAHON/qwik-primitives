182fac407183bc307e84e8d868aaf4ca4f0151f6
ZAHON <konrad.zahon@gmail.com>
2024-10-10 05:17:29 +0200

    feat(website): add `PageHeader` component into main layout

--- apps/website/src/routes/layout.tsx
+++ apps/website/src/routes/layout.tsx
@@ -1,11 +1,13 @@
 import type { RequestHandler } from '@builder.io/qwik-city';
 import { component$, Slot } from '@builder.io/qwik';
 import { Fonts } from '@/components/fonts';
+import { PageHeader } from '@/components/page-header';
 
 export default component$(() => {
   return (
     <>
       <Fonts />
+      <PageHeader />
       <Slot />
     </>
   );
