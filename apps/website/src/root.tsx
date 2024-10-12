4989f1a0ccba5fbb060d6bc66efa668de5e73aeb
ZAHON <konrad.zahon@gmail.com>
2024-10-12 06:17:13 +0200

    feat(website): add `Insights` component to `root.tsx`

--- apps/website/src/root.tsx
+++ apps/website/src/root.tsx
@@ -1,6 +1,7 @@
 import { component$ } from '@builder.io/qwik';
 import { isDev } from '@builder.io/qwik/build';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
+import { Insights } from '@builder.io/qwik-labs';
 import { RouterHead } from './components/router-head';
 import { themesScript } from '@/scripts/themes-script';
 
@@ -13,6 +14,7 @@ export default component$(() => {
         <meta charset="utf-8" />
         {!isDev && <link rel="manifest" href={`${import.meta.env.BASE_URL}manifest.json`} />}
         <RouterHead />
+        <Insights publicApiKey={import.meta.env.PUBLIC_QWIK_INSIGHTS_KEY} />
         <script dangerouslySetInnerHTML={`(${themesScript.toString()})()`} />
       </head>
       <body class="min-h-dvh overflow-x-hidden bg-background text-foreground selection:bg-primary-selection">
