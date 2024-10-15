965fbeaf56b4c1d6fa19a4cb63666a057f1f5257
ZAHON <konrad.zahon@gmail.com>
2024-10-15 22:27:06 +0200

    feat(primitives): add viewport meta tag in `root.tsx`

--- packages/primitives/src/root.tsx
+++ packages/primitives/src/root.tsx
@@ -3,6 +3,7 @@ export default () => {
     <>
       <head>
         <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Test</title>
       </head>
       <body></body>
