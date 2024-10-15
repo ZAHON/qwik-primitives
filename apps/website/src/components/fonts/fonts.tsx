c953887de290e4af67a8830425a8b72fbd29ca98
ZAHON <konrad.zahon@gmail.com>
2024-10-16 00:52:43 +0200

    feat(website): update `Fonts` component

--- apps/website/src/components/fonts/fonts.tsx
+++ apps/website/src/components/fonts/fonts.tsx
@@ -1,40 +1,8 @@
 import { component$, useStyles$ } from '@builder.io/qwik';
+import styles from './fonts.css?inline';
 
 export const Fonts = component$(() => {
-  useStyles$(`
-		@font-face {
-			font-family: 'Geist Sans';
-			font-style: normal;
-			font-display: swap;
-			font-weight: 400;
-			src: url(https://cdn.jsdelivr.net/fontsource/fonts/geist-sans@latest/latin-400-normal.woff2) format('woff2'), url(https://cdn.jsdelivr.net/fontsource/fonts/geist-sans@latest/latin-400-normal.woff) format('woff');
-		}
-
-		@font-face {
-			font-family: 'Geist Sans';
-			font-style: normal;
-			font-display: swap;
-			font-weight: 500;
-			src: url(https://cdn.jsdelivr.net/fontsource/fonts/geist-sans@latest/latin-500-normal.woff2) format('woff2'), url(https://cdn.jsdelivr.net/fontsource/fonts/geist-sans@latest/latin-500-normal.woff) format('woff');
-		}
-
-		@font-face {
-			font-family: 'Geist Sans';
-			font-style: normal;
-			font-display: swap;
-			font-weight: 700;
-			src: url(https://cdn.jsdelivr.net/fontsource/fonts/geist-sans@latest/latin-700-normal.woff2) format('woff2'), url(https://cdn.jsdelivr.net/fontsource/fonts/geist-sans@latest/latin-700-normal.woff) format('woff');
-		}
-
-		/* geist-mono-latin-400-normal */
-		@font-face {
-			font-family: 'Geist Mono';
-			font-style: normal;
-			font-display: swap;
-			font-weight: 400;
-			src: url(https://cdn.jsdelivr.net/fontsource/fonts/geist-mono@latest/latin-400-normal.woff2) format('woff2'), url(https://cdn.jsdelivr.net/fontsource/fonts/geist-mono@latest/latin-400-normal.woff) format('woff');
-		}
-	`);
+  useStyles$(styles);
 
   return <></>;
 });
