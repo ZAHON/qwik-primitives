91b138cab400fbffc4582ae524b6516c8d457922
ZAHON <konrad.zahon@gmail.com>
2024-10-26 00:11:28 +0200

    refactor(primitives): refactor `Link` component internal implementation

--- packages/primitives/src/components/link/link-root/link-root.tsx
+++ packages/primitives/src/components/link/link-root/link-root.tsx
@@ -1,30 +1,24 @@
 import type { LinkRootProps } from './link-root.types';
-import { component$, useComputed$, sync$, Slot } from '@builder.io/qwik';
+import { component$, Slot } from '@builder.io/qwik';
 
 /**
  * Contains the content for the link.
  * This component is based on the `a` element.
  */
 export const LinkRoot = component$<LinkRootProps>((props) => {
-  const { as, disabled, onClick$, ...others } = props;
-
-  const isDisabled = useComputed$(() => disabled);
-
-  const handleClickSync$ = sync$((event: MouseEvent, currentTarget: HTMLElement) => {
-    const disabled = currentTarget.getAttribute('aria-disabled') === 'true';
-    if (disabled) event.preventDefault();
-  });
+  const { as, href, disabled, ...others } = props;
 
   const Componet = as || 'a';
 
   return (
     <Componet
-      aria-disabled={isDisabled.value ? true : undefined}
+      role={disabled ? 'link' : undefined}
+      href={disabled ? undefined : href}
+      aria-disabled={disabled ? true : undefined}
       data-qwik-primitives-link-root=""
       data-scope="link"
       data-part="root"
-      data-disabled={isDisabled.value ? '' : undefined}
-      onClick$={[onClick$, handleClickSync$]}
+      data-disabled={disabled ? '' : undefined}
       {...others}
     >
       <Slot />
