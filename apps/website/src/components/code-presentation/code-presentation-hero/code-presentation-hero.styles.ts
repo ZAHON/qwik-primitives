c357fdb0fa619f6f99bde2399f8414c6a5ce6e7b
ZAHON <konrad.zahon@gmail.com>
2024-11-11 17:15:41 +0100

    feat(website): update `CodePresentation` component

--- apps/website/src/components/code-presentation/code-presentation-hero/code-presentation-hero.styles.ts
+++ apps/website/src/components/code-presentation/code-presentation-hero/code-presentation-hero.styles.ts
@@ -1,12 +1,33 @@
 import { cva } from 'class-variance-authority';
 
+// 'bg-gradient-to-tr',
+// 'from-[#18B6F6]',
+// 'to-[#AC7EF4]',
+
 export const codePresentationHeroStyles = cva(
   ['relative', 'px-4', 'w-full', 'flex', 'items-center', 'rounded-t-[var(--radius-4)]'],
   {
     variants: {
       heroWithGradient: {
         false: ['border-t', 'border-l', 'border-r', 'border-default-alpha-6'],
-        true: ['bg-gradient-to-tr', 'from-[#18B6F6]', 'to-[#AC7EF4]'],
+        true: [
+          'before:content-[""]',
+          'before:content-[""]',
+          'before:absolute',
+          'before:-z-10',
+          'before:inset-0',
+          'before:rounded-[inherit]',
+          'before:bg-gradient-to-tr',
+          'before:from-[#18B6F6]',
+          'before:to-[#AC7EF4]',
+
+          'after:content-[""]',
+          'after:absolute',
+          'after:-z-10',
+          'after:inset-0',
+          'after:rounded-[inherit]',
+          'after:[background-image:var(--dot-icon)]',
+        ],
       },
       heroPaddingY: {
         false: ['py-4'],
--- apps/website/src/components/code-presentation/code-presentation-hero/code-presentation-hero.tsx
+++ apps/website/src/components/code-presentation/code-presentation-hero/code-presentation-hero.tsx
@@ -11,7 +11,13 @@ export const CodePresentationHero = component$<CodePresentationHeroProps>((props
   const Component = component;
 
   return (
-    <div role="presentation" class={codePresentationHeroStyles({ heroWithGradient, heroPaddingY, heroJustify })}>
+    <div
+      role="presentation"
+      class={codePresentationHeroStyles({ heroWithGradient, heroPaddingY, heroJustify })}
+      style={{
+        '--dot-icon': `url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32' width='16' height='16' fill='none' %3E%3Ccircle fill='%2300073527' cx='10' cy='10' r='1.6257413380501518' %3E%3C/circle%3E%3C/svg%3E")`,
+      }}
+    >
       <Component />
     </div>
   );
