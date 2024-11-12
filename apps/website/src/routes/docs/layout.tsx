8fa565072110395da1551cf10bc6dc6719919a1b
ZAHON <konrad.zahon@gmail.com>
2024-11-12 13:14:46 +0100

    feat(website): update styles for docs layout

--- apps/website/src/routes/docs/layout.tsx
+++ apps/website/src/routes/docs/layout.tsx
@@ -3,7 +3,7 @@ import { DocsNav } from '@/components/docs-nav';
 
 export default component$(() => {
   return (
-    <div class="mx-auto flex max-w-screen-xl py-[calc(5.5rem+1px)]">
+    <div class="mx-auto flex max-w-screen-2xl py-[calc(5.5rem+1px)]">
       <div class="hidden w-64 shrink-0 lg:block">
         <aside class="fixed -mt-10 h-[calc(100%-3rem)] w-64 overflow-y-auto px-4 py-10">
           <DocsNav variant="desktop" />
