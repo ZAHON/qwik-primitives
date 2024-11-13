852a4d6fb7be54df61fd82ca7602874bbb8bd934
ZAHON <konrad.zahon@gmail.com>
2024-11-13 03:43:32 +0100

    feat(website): add `head` export in home page

--- apps/website/src/routes/index.tsx
+++ apps/website/src/routes/index.tsx
@@ -1,5 +1,16 @@
+import type { DocumentHead } from '@builder.io/qwik-city';
 import { component$ } from '@builder.io/qwik';
 
 export default component$(() => {
   return <></>;
 });
+
+export const head: DocumentHead = {
+  title: 'Qwik Primitives',
+  meta: [
+    {
+      name: 'description',
+      content: `Unstyled, accessible components for building high-quality design systems and web apps in Qwik.`,
+    },
+  ],
+};
