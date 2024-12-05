81fe055270be20d56ce621c776d129461c85d4b7
ZAHON <konrad.zahon@gmail.com>
2024-12-05 12:56:08 +0100

    feat(primitives): change context id for `TextField` component

--- packages/primitives/src/components/text-field/text-field-context/text-field-context.tsx
+++ packages/primitives/src/components/text-field/text-field-context/text-field-context.tsx
@@ -1,4 +1,4 @@
 import type { TextFieldContextValue } from './text-field-context.types';
 import { createContextId } from '@builder.io/qwik';
 
-export const TextFieldContext = createContextId<TextFieldContextValue>('qwik-primitives-context');
+export const TextFieldContext = createContextId<TextFieldContextValue>('qwik-primitives-text-field-context');
