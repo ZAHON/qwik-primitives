420d71f704924127cd3076169903b269459e2264
ZAHON <konrad.zahon@gmail.com>
2025-01-06 11:16:48 +0100

    build(website): add `astro-auto-import` astro integration

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -4,9 +4,10 @@ import { defineConfig } from 'astro/config';
 import qwikdev from '@qwikdev/astro';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import AutoImport from 'astro-auto-import';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   trailingSlash: 'never',
-  integrations: [qwikdev(), tailwind(), icon(), mdx()],
+  integrations: [qwikdev(), tailwind(), icon(), AutoImport({ imports: [] }), mdx()],
 });
