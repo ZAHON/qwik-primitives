a3b53af716b75de6a6fe8182b6b14028386af486
ZAHON <konrad.zahon@gmail.com>
2025-01-06 06:47:14 +0100

    build(website): add `@astrojs/mdx` astro integration

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -4,8 +4,9 @@ import { defineConfig } from 'astro/config';
 import qwikdev from '@qwikdev/astro';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
+import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   trailingSlash: 'never',
-  integrations: [qwikdev(), tailwind(), icon()],
+  integrations: [qwikdev(), tailwind(), icon(), mdx()],
 });
