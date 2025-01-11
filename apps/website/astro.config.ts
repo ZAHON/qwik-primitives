a4ba7635a7ff568398a2621d83ee46769a6d41f3
ZAHON <konrad.zahon@gmail.com>
2025-01-11 01:38:28 +0100

    build(website): remove `astro-auto-import` astro integration

--- apps/website/astro.config.ts
+++ apps/website/astro.config.ts
@@ -4,10 +4,9 @@ import { defineConfig } from 'astro/config';
 import qwikdev from '@qwikdev/astro';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import AutoImport from 'astro-auto-import';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
   trailingSlash: 'never',
-  integrations: [qwikdev(), tailwind(), icon(), AutoImport({ imports: [] }), mdx()],
+  integrations: [qwikdev(), tailwind(), icon(), mdx()],
 });
