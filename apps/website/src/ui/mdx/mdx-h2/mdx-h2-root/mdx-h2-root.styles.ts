8c1b71296bee11b706493e5e29bf35c4200650c8
ZAHON <konrad.zahon@gmail.com>
2025-01-15 00:21:42 +0100

    refactor(website): refactor `MDXH2Root` ui mdx component

--- apps/website/src/ui/mdx/mdx-h2/mdx-h2-root/mdx-h2-root.styles.ts
+++ apps/website/src/ui/mdx/mdx-h2/mdx-h2-root/mdx-h2-root.styles.ts
@@ -1,3 +1,3 @@
 import { cva } from 'class-variance-authority';
 
-export const MDXH2RootStyles = cva(['mt-12', 'mb-3', 'scroll-mt-16']);
+export const mdxH2RootStyles = cva(['mt-12', 'mb-3', 'scroll-mt-16']);
