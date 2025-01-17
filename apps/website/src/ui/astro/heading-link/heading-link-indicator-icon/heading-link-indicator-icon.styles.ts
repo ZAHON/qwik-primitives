608930a696f1d205f3a7f063234b5c34f633533f
ZAHON <konrad.zahon@gmail.com>
2025-01-18 00:52:37 +0100

    feat(website): update `HeadingLink` ui atro component

--- apps/website/src/ui/astro/heading-link/heading-link-indicator-icon/heading-link-indicator-icon.styles.ts
+++ apps/website/src/ui/astro/heading-link/heading-link-indicator-icon/heading-link-indicator-icon.styles.ts
@@ -1,14 +1,3 @@
 import { cva } from 'class-variance-authority';
 
-export const headingLinkIndicatorIconStyles = cva([''], {
-  variants: {
-    height: {
-      true: [''],
-      false: ['h-[var(--heading-link-indicator-icon-size)]'],
-    },
-    width: {
-      true: [''],
-      false: ['w-[var(--heading-link-indicator-icon-size)]'],
-    },
-  },
-});
+export const headingLinkIndicatorIconStyles = cva(['size-[var(--heading-link-indicator-icon-size)]']);
