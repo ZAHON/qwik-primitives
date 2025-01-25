34efaf0b24842bc86dc6d382cc2196780a48628d
ZAHON <konrad.zahon@gmail.com>
2025-01-25 09:41:39 +0100

    feat(website): update `MobileMenu` component

--- apps/website/src/components/mobile-menu/mobile-menu-body/mobile-menu-body.tsx
+++ apps/website/src/components/mobile-menu/mobile-menu-body/mobile-menu-body.tsx
@@ -1,9 +1,11 @@
 import { component$, Slot } from '@builder.io/qwik';
+import { twm } from '@/utilities/twm';
+import { scrollbarStyles } from '@/styles/shared/scrollbar';
 import { mobileMenuBodyStyles } from './mobile-menu-body.styles';
 
 export const MobileMenuBody = component$(() => {
   return (
-    <div class={mobileMenuBodyStyles()}>
+    <div class={twm(mobileMenuBodyStyles(), scrollbarStyles({ size: '1' }))}>
       <Slot />
     </div>
   );
