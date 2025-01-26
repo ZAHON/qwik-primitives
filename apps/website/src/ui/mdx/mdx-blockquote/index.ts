408e8252d780ceffe3dc6d6ef3404adb225c019a
ZAHON <konrad.zahon@gmail.com>
2025-01-26 23:09:37 +0100

    feat(website): add `MDXBlockquote` ui mdx component

---
+++ apps/website/src/ui/mdx/mdx-blockquote/index.ts
@@ -0,0 +1,1 @@
+export { default as MDXBlockquote } from './mdx-blockquote.astro';
--- apps/website/src/ui/mdx/mdx-components.ts
+++ apps/website/src/ui/mdx/mdx-components.ts
@@ -6,6 +6,7 @@ import { MDXUl } from './mdx-ul';
 import { MDXCodeBlock } from './mdx-code-block';
 import { MDXPreCode } from './mdx-pre-code';
 import { MDXA } from './mdx-a';
+import { MDXBlockquote } from './mdx-blockquote';
 
 export const MDXComponents = {
   h2: MDXH2,
@@ -16,4 +17,5 @@ export const MDXComponents = {
   pre: MDXCodeBlock,
   precode: MDXPreCode,
   a: MDXA,
+  blockquote: MDXBlockquote,
 };
